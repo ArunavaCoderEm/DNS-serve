8bc8e03fc332203e87e587f122bc596daf6d43c9
ArunavaCoderEm <acecankill@gmail.com>
2025-01-16 00:49:30 +0530

    whatever

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { startUdpServer, createConsoleLog } from "denamed";
 
-// Start the UDP server on port 9999
 startUdpServer((query: any): any => {
-  // Check if questions exists and is an array
-  if (query?.questions && Array.isArray(query.questions) && query.questions.length > 0) {
-    console.log(query.questions[0]?.name);  // Log the name if available
-  } else {
-    console.error("No valid questions array found in the query.");
+  try {
+    if (query?.type === 'OPT') {
+      console.log("Unsupported OPT type query received");
+      return;
+    }
+    
+    console.log(query?.queryResponse);
+    return query;
+  } catch (e) {
+    console.log(e);
   }
-  return;
 }, { port: 9999, log: createConsoleLog() });
 
-console.log("DNS server running on port 9999...");
+console.log('DNS server running on port 9999...');
